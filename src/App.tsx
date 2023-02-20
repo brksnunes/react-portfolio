bb63c985902ca6e8b9d28b358fe133e41ec742fb
Rodrigo Nunes <brksnunes@protonmail.com>
2023-02-20 19:14:52 -0300

    style: :lipstick: App.tsx
    Added main layout draft

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+import { Image, Flex, Text, Box } from '@chakra-ui/react'
+import './styles/main.css'
+
 function App() {
   return (
-    <div>
-      <h1>Hello World!!</h1>
-    </div>
+    <Flex justify='center' h='100vh' p={['1rem', '2rem', '5rem']}>
+      <Flex 
+        color='white' 
+        direction={['column', 'row']} 
+        bg='blackAlpha.400' 
+        backdropFilter="auto" 
+        backdropBlur="12px"
+        w={{ base: '90vw', md: '80vw', lg: '80vw' }}
+        border='1px' borderColor='red'
+      >
+        <Box w={['100%', '30vw', '20vw']} border='1px' borderColor='tomato' p={10}>
+          <Text>Box 1</Text>
+        </Box>
+        <Box w={['100%', '60vw', '60vw']} border='1px' borderColor='tomato' p={10}>
+          <Text>Box 3</Text>
+        </Box>
+      </Flex>
+    </Flex>
   )
 }
 
