a64a4dbb01790d8a47d81a06e493e88124743bbb
Rodrigo Nunes <brksnunes@protonmail.com>
2023-02-20 17:00:28 -0300

    feat: :tada: Project Start
    Created base project structure

---
+++ src/styles/theme.ts
@@ -0,0 +1,8 @@
+import { extendTheme } from '@chakra-ui/react'
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const customTheme = { }
+
+const theme = extendTheme({ customTheme })
+
+export default theme
