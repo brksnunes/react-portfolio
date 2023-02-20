1ee326e05251bb730c40bb8aee44a893c7310280
Rodrigo Nunes <brksnunes@protonmail.com>
2023-02-20 20:58:51 -0300

    style: :lipstick: App
    Changed some dimensions in order to be more responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import { Image, Flex, Text, Box } from '@chakra-ui/react'
-import './styles/main.css'
+import { Image, Flex, Text, Box } from '@chakra-ui/react';
+import Sidebar from './components/Sidebar';
+import './styles/main.css';
 
 function App() {
   return (
-    <Flex justify='center' h='100vh' p={['1rem', '2rem', '5rem']}>
+    <Flex justify='center' h='100vh' p={['1rem', '2rem', 'rem']}>
       <Flex 
         color='white' 
         direction={['column', 'row']} 
         bg='blackAlpha.400' 
         backdropFilter="auto" 
         backdropBlur="12px"
-        w={{ base: '90vw', md: '80vw', lg: '80vw' }}
-        border='1px' borderColor='red'
+        w={{ base: '90vw', md: '90vw', lg: '90vw' }}
       >
-        <Box w={['100%', '30vw', '20vw']} border='1px' borderColor='tomato' p={10}>
-          <Text>Box 1</Text>
+        <Box w={['100%', '30vw', '25vw']} p={5} bg='blackAlpha.500'>
+          <Sidebar />
         </Box>
-        <Box w={['100%', '60vw', '60vw']} border='1px' borderColor='tomato' p={10}>
+        <Box w={['100%', '60vw', '65vw']} p={10}>
           <Text>Box 3</Text>
         </Box>
       </Flex>
