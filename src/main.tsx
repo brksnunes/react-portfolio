56ca7d516f07d53816a8f95a932adeab5a85690a
Rodrigo Nunes <brksnunes@protonmail.com>
2023-02-21 13:08:06 -0300

    feat: :sparkles: React Query
    Added necessary modifications to 'main.tsx' in order to React Query to function

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { ChakraProvider, theme } from '@chakra-ui/react'
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider theme={ theme }>
-      <App />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={ theme }>
+        <App />
+      </ChakraProvider>
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
