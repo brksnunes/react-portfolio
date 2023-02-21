c81aa80abb647ee647d06bbb6e1f123967821508
Rodrigo Nunes <brksnunes@protonmail.com>
2023-02-21 19:29:34 -0300

    refactor: :art: Project card
    Refactored the props and added types

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -9,27 +9,27 @@ import {
   CardFooter,
   CardHeader,
 } from '@chakra-ui/react';
+import { Node } from '../interfaces/IGithubReturn';
 
-function ProjectCard({ data }) {
-  const { node } = data;
+function ProjectCard({ name, description, homepageUrl, url }: Node) {
   return (
     <Card bg='blackAlpha.500' color='white'>
-    <CardHeader>
-      <Heading size='sm'>{node.name}</Heading>
-    </CardHeader>
-    <CardBody>
-      <Text>{node.description}</Text>
-    </CardBody>
-    <CardFooter>
-    <Link href={node.url} isExternal>
-        <Button>Live</Button>
-      </Link>
-      <Link href={node.url} isExternal>
-        <Button>Code</Button>
-      </Link>
-    </CardFooter>
-  </Card>
-  )
+      <CardHeader>
+        <Heading size='sm'>{name}</Heading>
+      </CardHeader>
+      <CardBody>
+        <Text>{description}</Text>
+      </CardBody>
+      <CardFooter>
+        <Link href={homepageUrl ? homepageUrl : url} isExternal>
+          <Button>Live</Button>
+        </Link>
+        <Link href={url} isExternal>
+          <Button>Code</Button>
+        </Link>
+      </CardFooter>
+    </Card>
+  );
 }
 
-export default ProjectCard
+export default ProjectCard;
