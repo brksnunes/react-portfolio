d9e51c4387232c50fe56288a4912c5a946520939
Rodrigo Nunes <brksnunes@protonmail.com>
2023-02-21 23:37:31 -0300

    feat: :sparkles: Constants file
    added new file to contain all my skills

---
+++ src/utils/constants.ts
@@ -0,0 +1,103 @@
+export const frontend = [
+  {
+    name: 'HTML',
+    bg: '#E86128',
+    color: '#E2E8F0'
+  },
+  {
+    name: 'CSS',
+    bg: '#008ACE',
+    color: '#E2E8F0'
+  },
+  {
+    name: 'REACT',
+    bg: '#50BAD7',
+    color: '#2D3748'
+  },
+  {
+    name: 'REDUX',
+    bg: '#7548BA',
+    color: '#E2E8F0'
+  },
+  {
+    name: 'CONTEXT API',
+    bg: '#3A33D1',
+    color: '#E2E8F0'
+  },
+  {
+    name: 'JEST',
+    bg: '#97435B',
+    color: '#E2E8F0'
+  }
+];
+
+export const backend = [
+  {
+    name: 'JAVASCRIPT',
+    bg: '#EBD41C',
+    color: '#2D3748'
+  },
+  {
+    name: 'TYPESCRIPT',
+    bg: '#0074C2',
+    color: '#E2E8F0'
+  },
+  {
+    name: 'NODE.JS',
+    bg: '#8DC13E',
+    color: '#2D3748'
+  },
+  {
+    name: 'MYSQL',
+    bg: '#005E88',
+    color: '#E2E8F0'
+  },
+  {
+    name: 'MONGODB',
+    bg: '#10A84F',
+    color: '#E2E8F0'
+  },
+  {
+    name: 'SEQUELIZE',
+    bg: '#4DA9E4',
+    color: '#2D3748'
+  },
+  {
+    name: 'MONGOOSE',
+    bg: '#A41F22',
+    color: '#E2E8F0'
+  }
+];
+
+export const devops = [
+  {
+    name: 'DOCKER',
+    bg: '#2290E6',
+    color: '#E2E8F0'
+  },
+  {
+    name: 'LINUX',
+    bg: '#F7A903',
+    color: '#2D3748'
+  },
+  {
+    name: 'BASH',
+    bg: '#B9EC83',
+    color: '#2D3748'
+  },
+  {
+    name: 'AWS',
+    bg: '#F3941C',
+    color: '#2D3748'
+  },
+  {
+    name: 'DIGITAL OCEAN',
+    bg: '#017DF8',
+    color: '#E2E8F0'
+  },
+  {
+    name: 'PORTAINER',
+    bg: '#35D0F5',
+    color: '#2D3748'
+  }
+]
