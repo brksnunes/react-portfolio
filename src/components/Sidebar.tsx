b66497a35d5c040e06f006bb7da5d8a57ddccd77
Rodrigo Nunes <brksnunes@protonmail.com>
2023-02-21 23:38:08 -0300

    refactor: :art: Sidebar
    changed the way that the skills are rendered

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -8,6 +8,7 @@ import {
   Badge,
 } from '@chakra-ui/react';
 import { FiGithub, FiLinkedin, FiMail } from 'react-icons/fi';
+import { frontend, backend, devops } from '../utils/constants';
 function Sidebar() {
   return (
     <Flex direction='column' p={5}>
@@ -52,46 +53,38 @@ function Sidebar() {
         Get my CV
       </Button>
       <Divider mt='4' />
-      {/* // Skills */}
+
       <Text fontSize='sm' align='left' mt='7'>
         FRONTEND
       </Text>
       <Flex wrap='wrap' gap={'8px'} mt='2' justify={'center'}>
-        <Badge fontSize='0.9rem' bg='#f12' color='white'>
-          HTML
-        </Badge>
-        <Badge fontSize='0.9rem'>CSS</Badge>
-        <Badge fontSize='0.9rem'>REACT</Badge>
-        <Badge fontSize='0.9rem'>REDUX</Badge>
-        <Badge fontSize='0.9rem'>CONTEXT API</Badge>
-        <Badge fontSize='0.9rem'>JEST</Badge>
+        {frontend.map((item) => (
+          <Badge key={item.name} bg={item.bg} color={item.color} fontSize='0.9rem'>
+            {item.name}
+          </Badge>
+        ))}
       </Flex>
 
-      {/* // Skills */}
       <Text fontSize='sm' align='left' mt='7'>
         BACKEND
       </Text>
       <Flex wrap='wrap' gap={'8px'} mt='2' justify={'center'}>
-        <Badge fontSize='0.9rem'>JAVASCRIPT</Badge>
-        <Badge fontSize='0.9rem'>TYPESCRIPT</Badge>
-        <Badge fontSize='0.9rem'>NODE.JS</Badge>
-        <Badge fontSize='0.9rem'>MYSQL</Badge>
-        <Badge fontSize='0.9rem'>MONGODB</Badge>
-        <Badge fontSize='0.9rem'>SEQUELIZE</Badge>
-        <Badge fontSize='0.9rem'>MONGOOSE</Badge>
+      {backend.map((item) => (
+          <Badge key={item.name} bg={item.bg} color={item.color} fontSize='0.9rem'>
+            {item.name}
+          </Badge>
+        ))}
       </Flex>
 
-      {/* // Skills */}
       <Text fontSize='sm' align='left' mt='7'>
         DEVOPS
       </Text>
       <Flex wrap='wrap' gap={'8px'} mt='2' justify={'center'}>
-        <Badge fontSize='0.9rem'>DOCKER</Badge>
-        <Badge fontSize='0.9rem'>LINUX</Badge>
-        <Badge fontSize='0.9rem'>BASH</Badge>
-        <Badge fontSize='0.9rem'>AWS</Badge>
-        <Badge fontSize='0.9rem'>DIGITAL OCEAN</Badge>
-        <Badge fontSize='0.9rem'>PORTAINER</Badge>
+      {devops.map((item) => (
+          <Badge key={item.name} bg={item.bg} color={item.color} fontSize='0.9rem'>
+            {item.name}
+          </Badge>
+        ))}
       </Flex>
     </Flex>
   );
